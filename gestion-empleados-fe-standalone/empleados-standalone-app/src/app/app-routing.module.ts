f1bcfb5499bd218656e49ee37ffdb565cf5d6648
Christian Castillo <cyccov@gmail.com>
2024-02-01 21:01:28 -0600

    ajustes
    se agregan nuevos componentes y wildcards

--- gestion-empleados-fe-standalone/empleados-standalone-app/src/app/app-routing.module.ts
+++ gestion-empleados-fe-standalone/empleados-standalone-app/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaEmpleadosComponent } from './components/empleados/lista-empleados/lista-empleados.component';
+import {RegistrarEmpleadoComponent  } from './components/registrar-empleado/registrar-empleado.component';
+import { NotFound404Component } from './components/errors/not-found-404/not-found-404.component';
 
 const routes: Routes = [
   {path: 'empleados',component:ListaEmpleadosComponent}, //añade rutas
-  {path:'',redirectTo:'empleados',pathMatch:'full'}
+  {path:'',redirectTo:'empleados',pathMatch:'full'},
+  {path:'registrar-empleado',component: RegistrarEmpleadoComponent},
+  {path:'**',component: NotFound404Component} //wildcard
 ];
 
 @NgModule({
--- gestion-empleados-fe-standalone/empleados-standalone-app/src/app/app.module.ts
+++ gestion-empleados-fe-standalone/empleados-standalone-app/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ListaEmpleadosComponent } from './components/empleados/lista-empleados/lista-empleados.component';
+import { RegistrarEmpleadoComponent } from './components/registrar-empleado/registrar-empleado.component';
+import { FormsModule } from '@angular/forms';
+import { NotFound404Component } from './components/errors/not-found-404/not-found-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaEmpleadosComponent
+    ListaEmpleadosComponent,
+    RegistrarEmpleadoComponent,
+    NotFound404Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
