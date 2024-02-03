7551d40b28a9cb8867e11bb2051fcdb12180cf1d
Christian Castillo <cyccov@gmail.com>
2024-02-02 21:30:48 -0600

    commit

--- gestion-empleados-fe-standalone/empleados-standalone-app/src/app/components/registrar-empleado/registrar-empleado.component.ts
+++ gestion-empleados-fe-standalone/empleados-standalone-app/src/app/components/registrar-empleado/registrar-empleado.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Empleado } from '../../classes/empleado';
+import { EmpleadoService } from '../../services/empleado.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registrar-empleado',
@@ -10,7 +12,7 @@ export class RegistrarEmpleadoComponent implements OnInit{
 
   empleado : Empleado = new Empleado();
 
-  constructor(){}
+  constructor(private empleadoServicio:EmpleadoService, private router:Router){}
 
   ngOnInit(): void {
     console.log(this.empleado);  
@@ -19,5 +21,17 @@ export class RegistrarEmpleadoComponent implements OnInit{
   /** definimos metodos para usar en front*/
   onSubmit(){
     console.log(this.empleado);
+    
+  }
+
+  guardarEmpleado(){
+    this.empleadoServicio.registrarEmpleado(this.empleado).subscribe(dato => {
+      console.log(dato);
+      this.irALaListaDeEmpleados();
+    }, error => console.log(error));
+  }
+
+  irALaListaDeEmpleados(){
+    this.router.navigate(['/empleados']);
   }
 }
